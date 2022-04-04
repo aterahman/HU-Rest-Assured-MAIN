d2d48ab3e15aac0a2a8b57f908a3ae98eba131a7
Rahman <aterahman@deloitte.com>
2022-04-04 13:35:41 +0530

    log4 file

---
+++ src/test/java/log4j_file.java
@@ -0,0 +1,8 @@
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class log4j_file {
+    Logger logger = LogManager.getLogger(log4j_file.class.getName());
+
+}
